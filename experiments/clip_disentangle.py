75ba0d9c12dc09196dc4f4ecda9148837fa35967
moDal7 <marcodalmo96@gmail.com>
2023-06-16 21:35:36 +0200

    checkpoint save fix

--- experiments/clip_disentangle.py
+++ experiments/clip_disentangle.py
@@ -12,7 +12,7 @@ class EntropyLoss(nn.Module): # entropy loss as described in the paper 'Domain2V
 
     def forward(self, x):
         entropy = funct.softmax(x, dim=1) * funct.log_softmax(x, dim=1)
-        soft_sum = 1.0 * (entropy.sum()/x.size(0))
+        soft_sum = entropy.sum()/x.size(0)
         return soft_sum 
         
 class CLIPDisentangleExperiment: # See point 4. of the project
